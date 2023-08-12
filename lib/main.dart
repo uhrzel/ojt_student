d4debfb794f5af6d2ffcf42a047def07a65f212a
Arzel John Zolina <Arzeljrz17@gmail.com>
2023-08-12 17:16:29 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:ojt_student/register.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,214 +11,187 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Registration App',
-      home: RegistrationForm(),
+      debugShowCheckedModeBanner: false, // Set this property to false
+      title: 'Login Page',
+      home: LoginPage(),
     );
   }
 }
 
-class RegistrationForm extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  _RegistrationFormState createState() => _RegistrationFormState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _RegistrationFormState extends State<RegistrationForm> {
-  TextEditingController _studentIdNumberController = TextEditingController();
-  TextEditingController _firstNameController = TextEditingController();
-  TextEditingController _lastNameController = TextEditingController();
-  TextEditingController _contactNumberController = TextEditingController();
-  TextEditingController _startDateController = TextEditingController();
-  TextEditingController _courseController = TextEditingController();
-  TextEditingController _organizationController = TextEditingController();
-  TextEditingController _schoolYearController = TextEditingController();
-  TextEditingController _requiredHoursController = TextEditingController();
-  TextEditingController _addressController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  TextEditingController _confirmPasswordController = TextEditingController();
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void _login() async {
+    final url = Uri.parse('http://192.168.254.159/ojt_rms/student/signin.php');
+    final response = await http.post(url, body: {
+      'email': emailController.text,
+      'password': passwordController.text,
+    });
+
+    if (response.statusCode == 200) {
+      // Successful login, handle the response here
+      final responseData = json.decode(response.body);
+
+      // Check if the email exists in the API response
+      if (responseData['emailExists'] == true) {
+        // Email is valid, you can proceed with further logic
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Login Successful'),
+            duration: Duration(
+                seconds:
+                    2), // Duration for how long the SnackBar will be displayed
+          ),
+        );
+      } else {
+        // Email is not valid or not found in the database
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Invalid email or password'),
+            duration: Duration(
+                seconds:
+                    2), // Duration for how long the SnackBar will be displayed
+          ),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error: ${response.statusCode}'),
+          duration: Duration(
+              seconds:
+                  2), // Duration for how long the SnackBar will be displayed
+        ),
+      );
+      // Error occurred, handle the error here
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registration Form'),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _studentIdNumberController,
-              decoration: InputDecoration(
-                labelText: 'Student ID Number',
-                hintText: 'Enter your ID Number',
-                prefixIcon: Icon(Icons.numbers),
-                border: OutlineInputBorder(),
+      body: Container(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Logo and Text
+                  Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/logo.png', // Replace with your logo image path
+                          height: 200,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            'OJT RMS',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.amberAccent,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Align(
+                    // Use the Align widget to align the text to the left
+                    alignment: Alignment.centerLeft,
+                    child: TextButton(
+                      onPressed: () {
+                        // Implement forgot password logic
+                      },
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextButton(
+                    onPressed: _login, //for login
+                    style: ElevatedButton.styleFrom(
+                      // Apply the same style as the "Login" button
+                      primary: Colors.blueGrey,
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                    ),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegistrationForm()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      // Apply the same style as the "Login" button
+                      primary: Color.fromARGB(255, 104, 118, 110),
+                      padding: EdgeInsets.symmetric(vertical: 30),
+                    ),
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _firstNameController,
-              decoration: InputDecoration(
-                labelText: 'First Name',
-                hintText: 'Enter firstName',
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _lastNameController,
-              decoration: InputDecoration(
-                labelText: 'Last Name',
-                hintText: 'Enter your lastName',
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _contactNumberController,
-              decoration: InputDecoration(
-                labelText: 'Contact Number',
-                hintText: 'Enter your Contact number',
-                prefixIcon: Icon(Icons.phone),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _startDateController,
-              decoration: InputDecoration(
-                labelText: 'Start Date',
-                hintText: 'Enter start Date',
-                prefixIcon: Icon(Icons.calendar_month),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _courseController,
-              decoration: InputDecoration(
-                labelText: 'Course',
-                hintText: 'Enter your Course',
-                prefixIcon: Icon(Icons.list_alt),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _organizationController,
-              decoration: InputDecoration(
-                labelText: 'Organization',
-                hintText: 'Enter your Organization',
-                prefixIcon: Icon(Icons.list_outlined),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _schoolYearController,
-              decoration: InputDecoration(
-                labelText: 'School Year',
-                hintText: 'Enter School Year',
-                prefixIcon: Icon(Icons.calendar_today),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _requiredHoursController,
-              decoration: InputDecoration(
-                labelText: 'Required Hours',
-                hintText: 'Enter Required Training Hours',
-                prefixIcon: Icon(Icons.lock_clock),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _addressController,
-              decoration: InputDecoration(
-                labelText: 'Address',
-                hintText: 'Enter your Address',
-                prefixIcon: Icon(Icons.home),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your Email',
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your Password',
-                prefixIcon: Icon(Icons.lock),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: _confirmPasswordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Confirm Password',
-                hintText: 'Confirm your password',
-                prefixIcon: Icon(Icons.lock),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              height: 50,
-              width: 400, // Set the desired width
-              child: ElevatedButton(
-                onPressed: () {
-                  // Your button's onPressed logic
-                },
-                child: Text(
-                  'Register',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/register.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Set this property to false
+
+      title: 'Registration App',
+      home: RegistrationForm(),
+    );
+  }
+}
+
+class RegistrationForm extends StatefulWidget {
+  @override
+  _RegistrationFormState createState() => _RegistrationFormState();
+}
+
+class _RegistrationFormState extends State<RegistrationForm> {
+  TextEditingController _studentIdNumberController = TextEditingController();
+  TextEditingController _firstNameController = TextEditingController();
+  TextEditingController _lastNameController = TextEditingController();
+  TextEditingController _contactNumberController = TextEditingController();
+  TextEditingController _startDateController = TextEditingController();
+  TextEditingController _courseController = TextEditingController();
+  TextEditingController _organizationController = TextEditingController();
+  TextEditingController _schoolYearController = TextEditingController();
+  TextEditingController _requiredHoursController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+
+  void _registerUser() async {
+    // Fetch data from the form controllers
+    String studentIdNumber = _studentIdNumberController.text;
+    String firstName = _firstNameController.text;
+    String lastName = _lastNameController.text;
+    String contactNumber = _contactNumberController.text;
+    String startDate = _startDateController.text;
+    String course = _courseController.text;
+    String organization = _organizationController.text;
+    String schoolYear = _schoolYearController.text;
+    String requiredHours = _requiredHoursController.text;
+    String address = _addressController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    String confirmPassword = _confirmPasswordController.text;
+
+    // Check if passwords match
+    if (password != confirmPassword) {
+      // Show an error message that passwords do not match
+      return;
+    }
+
+    // Call the API to register the user
+    final response = await http.post(
+      Uri.parse('http://192.168.254.159/ojt_rms/student/signup.php'),
+      body: {
+        'student_id_number': studentIdNumber,
+        'first_name': firstName,
+        'last_name': lastName,
+        'contact_number': contactNumber,
+        'start_date': startDate,
+        'course_id': course,
+        'organization_id': organization,
+        'school_year': schoolYear,
+        'required_hours': requiredHours,
+        'address': address,
+        'user_email': email,
+        'user_password': password,
+      },
+    );
+
+    // Handle the API response here
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('successful registration'),
+          duration: Duration(
+              seconds:
+                  2), // Duration for how long the SnackBar will be displayed
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('failed registration !'),
+          duration: Duration(
+              seconds:
+                  2), // Duration for how long the SnackBar will be displayed
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registration Form'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _studentIdNumberController,
+              decoration: InputDecoration(
+                labelText: 'Student ID Number',
+                hintText: 'Enter your ID Number',
+                prefixIcon: Icon(Icons.numbers),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _firstNameController,
+              decoration: InputDecoration(
+                labelText: 'First Name',
+                hintText: 'Enter firstName',
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _lastNameController,
+              decoration: InputDecoration(
+                labelText: 'Last Name',
+                hintText: 'Enter your lastName',
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _contactNumberController,
+              decoration: InputDecoration(
+                labelText: 'Contact Number',
+                hintText: 'Enter your Contact number',
+                prefixIcon: Icon(Icons.phone),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _startDateController,
+              decoration: InputDecoration(
+                labelText: 'Start Date',
+                hintText: 'Enter start Date',
+                prefixIcon: Icon(Icons.calendar_month),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _courseController,
+              decoration: InputDecoration(
+                labelText: 'Course',
+                hintText: 'Enter your Course',
+                prefixIcon: Icon(Icons.list_alt),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _organizationController,
+              decoration: InputDecoration(
+                labelText: 'Organization',
+                hintText: 'Enter your Organization',
+                prefixIcon: Icon(Icons.list_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _schoolYearController,
+              decoration: InputDecoration(
+                labelText: 'School Year',
+                hintText: 'Enter School Year',
+                prefixIcon: Icon(Icons.calendar_today),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _requiredHoursController,
+              decoration: InputDecoration(
+                labelText: 'Required Hours',
+                hintText: 'Enter Required Training Hours',
+                prefixIcon: Icon(Icons.lock_clock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _addressController,
+              decoration: InputDecoration(
+                labelText: 'Address',
+                hintText: 'Enter your Address',
+                prefixIcon: Icon(Icons.home),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Enter your Email',
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter your Password',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              controller: _confirmPasswordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                hintText: 'Confirm your password',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 50,
+              width: 400, // Set the desired width
+              child: ElevatedButton(
+                onPressed: () {
+                  // Your button's onPressed logic
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
