6aa100cad24776c5c1b0ffbb661c3663ee9b269f
Arzel John Zolina <Arzeljrz17@gmail.com>
2023-08-21 21:29:35 +0800

    hahayst

--- lib/attendance_scanner_afternoon.dart
+++ lib/attendance_scanner_afternoon.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; // Import the http package
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:http/http.dart' as http;
 
 class QRCodeScannerScreenAfternoon extends StatefulWidget {
   @override
@@ -19,89 +19,17 @@ class _QRCodeScannerScreenState extends State<QRCodeScannerScreenAfternoon> {
     super.dispose();
   }
 
-  Future<void> sendAttendanceLog(
-    String attendanceLog,
-    String studentId,
-    String attendanceDate,
-    String attendanceTime,
-    String coordinatorId,
-    String organizationId,
-  ) async {
-    final apiUrl =
-        'http://192.168.254.159/ojt_rms/student/attendance_afternoon_create.php';
-
-    try {
-      final response = await http.get(
-        Uri.parse(apiUrl),
-        headers: {
-          'attendance_log': attendanceLog,
-          'student_id': studentId,
-          'attendance_date': attendanceDate,
-          'attendance_time': attendanceTime,
-          'coordinator_id': coordinatorId,
-          'organization_id': organizationId,
-        },
-      );
-
-      if (response.statusCode == 200) {
-        print(response.statusCode);
-        print(response.body);
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Attendance Logged'),
-              content: Text('Attendance data has been logged.'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
-      } else {
-        // Display error dialog
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Error'),
-              content: Text('Failed to log attendance.'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('OK'),
-                ),
-              ],
-            );
-          },
-        );
-      }
-    } catch (e) {
-      // Handle exception
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Error'),
-            content: Text('An error occurred: $e'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
+  void sendAttendanceData(String attendanceData) async {
+    final url = Uri.parse(
+        'http://192.168.254.159/ojt_rms/student/attendance_afternoon_create.php'); // Replace with your API URL
+    final response = await http.get(url); // Make a GET request to your API
+    if (response.statusCode == 200) {
+      print(response.statusCode);
+      // Handle the API response here
+      print(response.body);
+    } else {
+      // Handle error
+      print('Failed to send attendance data');
     }
   }
 
@@ -118,39 +46,18 @@ class _QRCodeScannerScreenState extends State<QRCodeScannerScreenAfternoon> {
         ),
         onQRViewCreated: (controller) {
           this.controller = controller;
-          // Inside the onQRViewCreated callback
           controller.scannedDataStream.listen((scanData) {
             if (!isScanned && scanData != null) {
               isScanned = true;
-
-              // Extract data from the scanned QR code (replace with actual values)
-              Map<String, String> qrData =
-                  Uri.splitQueryString(scanData.code ?? '');
-
-              String scannedAttendanceLog = qrData['attendance_log'] ?? '';
-              String scannedStudentId = qrData['student_id'] ?? '';
-              String scannedAttendanceDate = qrData['attendance_date'] ?? '';
-              String scannedAttendanceTime = qrData['attendance_time'] ?? '';
-              String scannedCoordinatorId = qrData['coordinator_id'] ?? '';
-              String scannedOrganizationId = qrData['organization_id'] ?? '';
-
-              // Send attendance data to the API
-              sendAttendanceLog(
-                scannedAttendanceLog,
-                scannedStudentId,
-                scannedAttendanceDate,
-                scannedAttendanceTime,
-                scannedCoordinatorId,
-                scannedOrganizationId,
-              );
-
+              // Send the scanned data to the API
+              sendAttendanceData(scanData.code ?? '');
               // Display the scanned data using a dialog
               showDialog(
                 context: context,
                 builder: (BuildContext context) {
                   return AlertDialog(
                     title: Text('Scanned QR Code'),
-                    content: Text('Scanned Data: ${scanData.code ?? ""}'),
+                    content: Text('Scanned Data: ${scanData.code}'),
                     actions: [
                       TextButton(
                         onPressed: () {
@@ -165,7 +72,6 @@ class _QRCodeScannerScreenState extends State<QRCodeScannerScreenAfternoon> {
               );
             }
           });
-
           controller.resumeCamera();
         },
       ),
